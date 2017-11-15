881a0ac8c41ff38ce68ea0d2df89d918ac5589cf
4010c11 <4010c11@carambola.ece.uprm.edu>
2017-11-15 16:14:14 -0400

    Changed the location and name of the frame.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,10 +4,10 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Ken");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(900, 400);
+		myFrame.setLocation(850, 400);
 		myFrame.setSize(200, 200);
-		MyPanelClass myPanel = new MyPanelClass();
-        myFrame.getContentPane().add(myPanel);
+		//MyPanelClass myPanel = new MyPanelClass();
+       // myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 		
 		
